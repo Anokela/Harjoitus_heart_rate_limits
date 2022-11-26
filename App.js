a70d18c61a4335e4fb4ce28bea9f66de07573aae
Aleksi Nokela <n0noal00@students.oamk.fi>
2022-11-26 19:02:02 +0200

    push to repository

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+ 
+  const [age, setAge] = useState('');
+  const [lower, setLower] = useState(0);
+  const [upper,setUpper] = useState(0);
+
+  function calculate(text) {
+    setAge(text);
+    setLower(((220 - text) * 0.65));
+    setUpper(((220 - text) * 0.85));
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.field}>Age:</Text>
+      <TextInput style={styles.field} value={age} onChangeText={text => calculate(text)}
+      keyboardType='numeric'></TextInput>
+      <Text style={styles.field}>Hr limits:</Text>
+      <Text style={styles.field}>{lower.toFixed(0)} - {upper.toFixed(0)}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 10,
+  },
+  field: {
+    marginBottom: 10,
   },
 });
